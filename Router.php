ab554fcfb1b7dd38da158e0c8caf89a45bc7aa73
Bryan Chaacha <morocho.12chachas@gmail.com>
2022-03-04 23:38:14 -0500

    Cambio de configuracion en Router

--- Router.php
+++ Router.php
@@ -27,19 +27,16 @@ class Router
         // $rutas_protegidas = ['/admin', '/propiedades/crear', '/propiedades/actualizar', '/propiedades/eliminar', '/vendedores/crear', '/vendedores/actualizar', '/vendedores/eliminar'];
 
         // $auth = $_SESSION['login'] ?? null;
-        //
-        $currentUrl = ($_SERVER['REQUEST_URI'] === '') ? '/' :  $_SERVER['REQUEST_URI'] ;
+
+        $currentUrl = $_SERVER['REQUEST_URI'] === ''? '/':$_SERVER['REQUEST_URI'] ;
         $method = $_SERVER['REQUEST_METHOD'];
-            
-        //dividimos la URL actual cada vez que exista un '?' eso indica que se están pasando variables por la url
-        $splitURL = explode('?', $currentUrl);
-        // debuguear($splitURL);
-         
+
         if ($method === 'GET') {
-            $fn = $this->getRoutes[$splitURL[0]] ?? null; //$splitURL[0] contiene la URL sin variables 
+            $fn = $this->getRoutes[$currentUrl] ?? null;
         } else {
-          $fn = $this->postRoutes[$splitURL[0]] ?? null;
+            $fn = $this->postRoutes[$currentUrl] ?? null;
         }
+
 
         if ( $fn ) {
             // Call user fn va a llamar una función cuando no sabemos cual sera
